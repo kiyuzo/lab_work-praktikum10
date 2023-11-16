e74a29bfe739a5eb05557ff10b1fcc9f74877a75
Rama Andhika Pratama <137267391+kiyuzo@users.noreply.github.com>
2023-11-16 08:21:13 +0700

    prakpemrog2.cpp

---
+++ prakpemrog2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {10, 25, 44, 50, 41, 42, 49, 10, 15, 38, 7, 45, 12, 15, 41, 48, 40, 44, 22, 39};
+    int dataSize = sizeof(arr)/sizeof(arr[0]);
+
+    // INPUTS THE DESIGNATED NUMBER TO BE GREATER THAN
+    int key;
+    cout << "Please insert what number should be greater than : ";
+    cin >> key;
+
+    // SEARCHING THE NUMBERS
+    int count = 0;
+    for(int i = 0; i < dataSize; i++){
+        if(arr[i] > key){
+            count++;
+        }
+    }
+
+    if(count == 0){
+        cout << "No Data Was Found";
+    }
+    else{
+        cout << "DATA FOUND! There are " << count << " data(s) of " << key << "(s)" << endl;
+        cout << "The data(s) are : ";
+        for(int i = 0; i < dataSize; i++){
+            if(arr[i] > key){
+                cout << arr[i] << ", ";
+            }
+        }
+    }
+}
